e789dceb01128628b84ea429cc756159f4f5c37c
Gang Wen <73705196+gawen-ikea@users.noreply.github.com>
2024-07-28 18:52:47 +0200

    refactor structure to publish package

--- index.js
+++ index.js
@@ -2,25 +2,6 @@ const { buildPlatformArch } = require('./install/check')
 
 const runtimePlatform = buildPlatformArch();
 
-let vips;
-
-try {
-    vips = require(`./build/Release/js-lib-vips-${runtimePlatform}.node`);
-
-    console.log('js-lib-vips', vips);
-
-    console.log("result", vips.hello());
-    console.log("add", vips.add(12,13));
-    
-    const sampleClass = new vips.ActualClass(4.3);
-    console.log('Testing class initial value: ', sampleClass.getValue());
-    console.log("After adding 3.3 ", sampleClass.add(3.3));
-
-} catch (err) {
-    throw new Error(err);
-}
-
-
-
+const vips = require('bindings')(`js-lib-vips-${runtimePlatform}.node`);
 
 module.export = vips
---
+++ test/js/js-lib-vips-test.js
@@ -0,0 +1,10 @@
+const vips = require("../../index.js");
+
+console.log('js-lib-vips', vips);
+
+console.log("result", vips.hello());
+console.log("add", vips.add(12,13));
+
+const sampleClass = new vips.ActualClass(4.3);
+console.log('Testing class initial value: ', sampleClass.getValue());
+console.log("After adding 3.3 ", sampleClass.add(3.3));
